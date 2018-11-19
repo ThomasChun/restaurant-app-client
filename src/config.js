f54064beca7d2070a00aaaf8005637469655aae2
Thomas Chun <tchun2055@gmail.com>
2018-11-19 14:17:40 -0800

    update

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const CREATE_REACT_APP_BASE_URL = process.env.CREATE_REACT_APP_BASE_URL || 'https://localhost:8080';
+export const REACT_APP_API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://localhost:8080';
