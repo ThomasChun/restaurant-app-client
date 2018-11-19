edff5bfb70c10296f4dfac27f2f4f0e140ed6809
Thomas Chun <tchun2055@gmail.com>
2018-11-19 07:23:16 -0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import restaurantsReducer from './reducers/restaurants';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import RestaurantList from './components/restaurant-list';
+import AddRestaurant from './components/add-restaurant';
+import Randomize from './components/randomize';
+import {addRestaurant} from './actions/restaurants';
+
+const store = createStore(restaurantsReducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <div>
+      <AddRestaurant onSubmitRestaurant={addRestaurant}/>
+      <RestaurantList />
+      <Randomize />
+    </div>
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
