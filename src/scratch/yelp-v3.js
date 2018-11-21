1ff0a2be54fe9548e52baab7ba53974b24eeaf46
Thomas Chun <tchun2055@gmail.com>
2018-11-21 13:25:28 -0800

    added yelp-fusion scratch file

---
+++ src/scratch/yelp-v3.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const yelp = require('yelp-fusion');
+const client = yelp.client('2r0FTJAbD1S6Plv0L5qhnPrh2LO24biSJ__-yiYESyzPlRcU5aaCqkTSk-Gi2CA42MjBx4wNNHVWeSUHJDJlZ6p7mbj7pOFpnbxfGcRDS-gTN0Rcr317Jhf1e-7dW3Yx');
+
+client.search({
+  term:'aca grill',
+  location: 'los angeles, ca'
+}).then(response => {
+  const restaurantsSearchResult = response.jsonBody.businesses;
+  console.log(`Your search has returned ${restaurantsSearchResult.length} results!`);
+  let filteredSearchData = restaurantsSearchResult.map(restaurant => {
+    return {
+      name: restaurant.name,
+      id: restaurant.id,
+      rating: restaurant.rating,
+      price: restaurant.price,
+      address: `${restaurant.location.address1}, ${restaurant.location.city}, ${restaurant.location.state} ${restaurant.location.zip_code}`,
+      // image: restaurant.image_url,
+      // url: restaurant.url,
+    }
+  })
+  console.log(filteredSearchData);
+}).catch(e => {
+  console.log(e);
+});
+
+// client.search({
+//   term:'aca grill',
+//   location: 'los angeles, ca'
+// }).then(response => {
+//   console.log('total results', response.jsonBody.businesses.length);
+//   console.log(response.jsonBody.businesses[0].name);
+//   console.log(response.jsonBody.businesses[0].id);
+//   console.log(response.jsonBody.businesses[0].alias);
+//   console.log(response.jsonBody.businesses[0].rating);
+//   console.log(response.jsonBody.businesses[0].price);
+//   console.log(response.jsonBody.businesses[0].location.address1);
+//   console.log(response.jsonBody.businesses[0].location.city);
+//   console.log(response.jsonBody.businesses[0].location.zip_code);
+//   console.log(response.jsonBody.businesses[0].location.display_address);
+//   console.log(response.jsonBody.businesses[0].display_phone);
+// }).catch(e => {
+//   console.log(e);
+// });
