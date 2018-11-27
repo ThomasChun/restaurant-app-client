4a4eb4eeeec389ba66875c1f55ff05ed28e0e5ed
Thomas Chun <tchun2055@gmail.com>
2018-11-27 00:59:26 -0800

    added yelp-api integration

--- src/components/yelp-search-results.js
+++ src/components/yelp-search-results.js
@@ -18,30 +18,39 @@ class YelpSearchResults extends React.Component {
   }
 
   render() {
-    const searchResults = this.props.yelpSearchResults[0].searchResult.slice(0, 5);
-    const searchResult = searchResults.map((restaurant, index) => {
+    console.log(this.props.yelpSearchResults[0])
+    if (this.props.yelpSearchResults[0] === undefined || this.props.yelpSearchResults[0].searchResult.length === 0) {
       return (
-        <li key={index} id={restaurant.id} className='yelp-search-result'>
-          <img src={restaurant.image} alt={restaurant.name} className='yelp-search-result-img'></img>
-          <button className='add-button' id={restaurant.id} onClick={e => this.onClick(e)}>+ Add Restaurant!</button>
-          <ul>
-            <li><b>Name:</b> <a href={restaurant.url} target='blank'>{restaurant.name}</a></li>
-            <li><b>Address:</b>  {restaurant.address}</li>
-            <li><b>Rating:</b>  {restaurant.rating}</li>
-            <li><b>Price:</b>  {restaurant.price}</li>
-            <li><b>Yelp! Review Count:</b>  {restaurant.reviewCount}</li>
-          </ul>
-        </li>
+        <div>
+          <h3>Search Yelp! for restaurants to your collection above!</h3>
+        </div>
       )
-    })
-    return (
-      <div>
-        <h3>Yelp! Search Results:</h3>
-        <ul>
-          {searchResult}
-        </ul>
-      </div>
-    );
+    } else {
+      const searchResults = this.props.yelpSearchResults[0].searchResult.slice(0, 5);
+      const searchResult = searchResults.map((restaurant, index) => {
+        return (
+          <li key={index} id={restaurant.id} className='yelp-search-result'>
+            <img src={restaurant.image} alt={restaurant.name} className='yelp-search-result-img'></img>
+            <button className='add-button' id={restaurant.id} onClick={e => this.onClick(e)}>+ Add Restaurant!</button>
+            <ul>
+              <li><b>Name:</b> <a href={restaurant.url} target='blank'>{restaurant.name}</a></li>
+              <li><b>Address:</b>  {restaurant.address}</li>
+              <li><b>Rating:</b>  {restaurant.rating}</li>
+              <li><b>Price:</b>  {restaurant.price}</li>
+              <li><b>Yelp! Review Count:</b>  {restaurant.reviewCount}</li>
+            </ul>
+          </li>
+        )
+      })
+      return (
+        <div>
+          <h3>Yelp! Search Results:</h3>
+          <ul>
+            {searchResult}
+          </ul>
+        </div>
+      );
+    }
   }
 }
 
